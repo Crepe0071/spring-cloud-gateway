78ed69e8dd6004be73bb260e06675ecaa3df9782
이준규 <103472310+ljk0071@users.noreply.github.com>
2025-01-03 02:48:26 +0900

    feature: add profile seperated ip addr

--- src/main/java/com/example/apigateway/config/WasStateManager.java
+++ src/main/java/com/example/apigateway/config/WasStateManager.java
@@ -1,16 +1,31 @@
 package com.example.apigateway.config;
 
-import org.springframework.stereotype.Component;
-
+import jakarta.annotation.PostConstruct;
 import java.util.Collections;
 import java.util.Set;
 import java.util.concurrent.ConcurrentSkipListSet;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class WasStateManager {
 
     private final Set<String> upInstances = new ConcurrentSkipListSet<>();
     private final Set<String> downInstances = new ConcurrentSkipListSet<>();
+    private final Environment env;
+
+    @PostConstruct
+    public void init() {
+        for (String activeProfile : env.getActiveProfiles()) {
+            if ("local".equals(activeProfile)) {
+                upInstances.add("localhost:8082");
+            } else if ("dev".equals(activeProfile)) {
+                upInstances.add("172.31.19.11:8080");
+            }
+        }
+    }
 
     public void markUp(String wasAddress) {
         downInstances.remove(wasAddress);
@@ -34,7 +49,7 @@ public class WasStateManager {
         if (upInstances.isEmpty()) {
             throw new IllegalStateException("No up instances available");
         }
-        return  upInstances.iterator().next();
+        return upInstances.iterator().next();
     }
 }
 
