8004a494c8c3010f719be7cb2ee550dec775499c
ljk0071 <ssobbs13@naver.com>
2025-01-10 09:02:18 +0900

    refactor: tpms to domain static string

--- src/main/java/com/example/apigateway/config/GatewayConfig.java
+++ src/main/java/com/example/apigateway/config/GatewayConfig.java
@@ -10,15 +10,17 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class GatewayConfig {
 
+	private static final String DOMAIN = "tpms";
+
 	@Bean
 	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
 		return builder.routes()
 				   .route(WAS_ROUTE.getValue(),
 					   r -> r.path("/")
-								.filters(f -> f.setPath("/tpms"))
+								.filters(f -> f.setPath(String.format("/%s", DOMAIN)))
 								.uri(String.format("lb://%s", WAS_ROUTE.getValue())))
 				   .route(WAS_ROUTE.getValue(),
-					   r -> r.path("/tpms/**")
+					   r -> r.path(String.format("/%s/**", DOMAIN))
 								.uri(String.format("lb://%s", WAS_ROUTE.getValue())))
 				   .build();
 	}
